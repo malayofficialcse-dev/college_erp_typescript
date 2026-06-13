2c6c1984eacfdecd924ddb9729a1f5c0c7ade0d6
malayofficialcse-dev <malay.official.cse@gmail.com>
2026-06-13 19:41:39 +0530

    feat: add Redis service and persistent volume claim to Kubernetes deployment

--- backend/config/db.config.ts
+++ backend/config/db.config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
-  const mongoUri = "mongodb://localhost:27017/erp";
+  const mongoUri = process.env.MONGO_URI_ATLAS || "mongodb://localhost:27017/erp";
   try {
     await mongoose.connect(mongoUri);
     console.log("MongoDB connected");
